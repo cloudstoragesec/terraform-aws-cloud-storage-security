a92cbe4efc955319c0f727b2602948e0c8aff88d
Kara <kara@curai.com>
2026-03-18 15:49:10 -0700

    Try bumping aws provider version

--- main.tf
+++ main.tf
@@ -1,8 +1,9 @@
 terraform {
   required_providers {
     aws = {
-      source  = "hashicorp/aws"
-      version = ">= 4.0"
+      source      = "hashicorp/aws"
+      version     = "6.37.0"
+      constraints = ">= 4.0.0, ~> 6.37.0"
     }
     awscc = {
       source  = "hashicorp/awscc"
